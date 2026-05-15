bc76a375b3385df3ef27067ebcdfb797d9fb9c34
Leonardo Sanchez <leonardosanchez119@gmail.com>
2026-05-15 09:26:50 -0300

    agrego clase Backtracking para prueba y archivo en markdown para agregar teoria

---
+++ tp5/Backtracking.java
@@ -0,0 +1,21 @@
+package tp5;
+
+public class Backtracking {
+
+    public void resolver() {
+
+        Estado inicial = new Estado();
+
+        backtrack(inicial);
+    }
+
+    private void backtrack(Estado estado) {
+
+       /*  if (esSolucion(estado)) {
+
+        } else {
+
+            // generar hijoss
+        } */
+    }
+}
