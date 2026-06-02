01e670f373371c9db71527de45534e350bb46218
Leonardo Sanchez <leonardosanchez119@gmail.com>
2026-06-01 21:09:14 -0300

    sigo intentando hacer el 1

--- tp5/Backtracking.java
+++ tp5/Backtracking.java
@@ -1,21 +1,41 @@
 package tp5;
 
-public class Backtracking {
+import java.util.ArrayList;
+
+import tp4.GrafoDirigido;
+
+public class Backtracking<T> {
+
+    private ArrayList<T> solucionActual;
+    private ArrayList<T> mejorSolucion;
+
+    public Backtracking() {
+        solucionActual = new ArrayList<>();
+        mejorSolucion = new ArrayList<>();
+    }
 
     public void resolver() {
 
-        Estado inicial = new Estado();
+        GrafoDirigido<T> grafo = new GrafoDirigido<>();
 
-        backtrack(inicial);
+        backtrack(grafo);
     }
 
-    private void backtrack(Estado estado) {
+    private void backtrack(GrafoDirigido<T> estado) {
+        /*
+         * si llegué a la salida:
+         * si este camino es mejor:
+         * actualizar mejor solución
+         * seguir buscando
+         */
 
-       /*  if (esSolucion(estado)) {
-
-        } else {
-
-            // generar hijoss
-        } */
+        /*
+            * if (esSolucion(estado)) {
+            * 
+            * } else {
+            * 
+            * // generar hijoss
+            * }
+         */
     }
 }
